a5c651dfa3c019482c980fcafd32cfdaea615fb8
rayGreycloud <rayGreycloud@gmail.com>
2016-12-16 09:51:26 -0800

    Refactor filter using arrow function

--- array-methods-practice.js
+++ array-methods-practice.js
@@ -18,15 +18,14 @@ const flavours = ['Chocolate Chip', 'Kulfi', 'Caramel Praline', 'Chocolate', 'Bu
 const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
 
 // Array.prototype.filter()
-//
 console.log("Question 1. Filter the list of inventors for those who were born in the 1500's");
-const fifteenth = inventors.filter(function (inventor) {
-  return (inventor.year >= 1500 && inventor.year < 1600);
-});
+const fifteenth = inventors.filter(inventor => inventor.year >= 1500 && inventor.year < 1600);
 console.log(fifteenth);
 
 // Array.prototype.map()
-// 2. Give us an array of the inventory first and last names
+console.log("Question 2. Create an array of the inventors first and last names");
+
+
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
