5e93c7c409bb5a58d28aebeb77b4f3cea2ac06de
rayGreycloud <rayGreycloud@gmail.com>
2016-12-18 12:23:37 -0800

    Clean up starter code

--- array-methods-practice2.js
+++ array-methods-practice2.js
@@ -1,14 +1,22 @@
 // Wes Bos - Array Cardio Day 2
 // Array Methods Practice
 
-// Some Data
+// Array.prototype.some()
 const people = [
   { name: 'Wes', year: 1988 },
   { name: 'Kait', year: 1986 },
   { name: 'Irv', year: 1970 },
   { name: 'Lux', year: 2015 },
 ];
+// Ex.1 Is at least one person 19?
+const isAdult = people
+console.log({isAdult});
 
+// Array.prototype.every()
+// Ex.2 Is everyone 19?
+
+
+// Array.prototype.find()
 const comments = [
   { text: 'Love this!', id: 523423 },
   { text: 'Super good', id: 823423 },
@@ -16,30 +24,13 @@ const comments = [
   { text: 'Ramen in my fav food ever', id: 123523 },
   { text: 'Nice Nice Nice!', id: 542328 }
 ];
-
-// Some and Every Checks
-// Array.prototype.some()
-// Ex.1 Is at least one person 19?
-
-console.log({isAdult});
-
-// Array.prototype.every()
-// Ex.2 Is everyone 19?
-
-console.log({allAdults});
-
-// Array.prototype.find()
 // Ex.3 Find the comment with ID = 823423
 
-console.log(comment);
 
 // Array.prototype.findIndex()
 // Ex. 4 Find the comment with ID
 
-
 // Ex.5 Delete the comment with ID = 823423
-
-console.log(index);
 
 // Comments.splice(index, 1);
 // Ex.6
