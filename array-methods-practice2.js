e90393f48bebe953dd27dae9705cc1f44d35001e
rayGreycloud <rayGreycloud@gmail.com>
2016-12-18 08:27:03 -0800

    Add WesBos array methods practice 2

---
+++ array-methods-practice2.js
@@ -0,0 +1,45 @@
+// Wes Bos - Array Cardio Day 2
+// Array Methods Practice
+
+// Some Data
+const people = [
+  { name: 'Wes', year: 1988 },
+  { name: 'Kait', year: 1986 },
+  { name: 'Irv', year: 1970 },
+  { name: 'Lux', year: 2015 },
+];
+
+const comments = [
+  { text: 'Love this!', id: 523423 },
+  { text: 'Super good', id: 823423 },
+  { text: 'You are the best', id: 2039842 },
+  { text: 'Ramen in my fav food ever', id: 123523 },
+  { text: 'Nice Nice Nice!', id: 542328 }
+];
+
+// Some and Every Checks
+// Array.prototype.some()
+// Ex.1 Is at least one person 19?
+
+console.log({isAdult});
+
+// Array.prototype.every()
+// Ex.2 Is everyone 19?
+
+console.log({allAdults});
+
+// Array.prototype.find()
+// Ex.3 Find the comment with ID = 823423
+
+console.log(comment);
+
+// Array.prototype.findIndex()
+// Ex. 4 Find the comment with ID
+
+
+// Ex.5 Delete the comment with ID = 823423
+
+console.log(index);
+
+// Comments.splice(index, 1);
+// Ex.6
